49af456fcb718045abae30f16d9177a18833238f
Sagar Padwal <padwalsagar@yahoo.com>
2023-07-04 19:52:28 +0530

    Initial commit
    Initial code for sample web application project

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace myContainerizedApp.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+        public DbSet<Models.Student>? Students { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using myContainerizedApp.Data;
+
+namespace myContainerizedApp.Pages;
+
+public class IndexModel : PageModel
+{
+    public string StudentName { get; private set; } = "PageModel in C#";
+    private readonly ILogger<IndexModel> _logger;
+    private readonly SchoolContext _context;
+
+    public IndexModel(ILogger<IndexModel> logger, SchoolContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public void OnGet()
+    {
+        var s = _context.Students?.Where(d => d.ID == 1).FirstOrDefault();
+        this.StudentName = $"{s?.FirstMidName} {s?.LastName}";
+    }
+}
